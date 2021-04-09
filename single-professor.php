4adb01b0374f7089fdd1eaf7087e6e7320092bbc
Irina <irensaywhen@gmail.com>
2021-04-09 13:20:22 +0300

    Page banner function to avoid code duplication

--- single-professor.php
+++ single-professor.php
@@ -4,21 +4,8 @@
   while(have_posts()) {
     the_post(); 
     
-    $page_banner_image = get_field('page_banner_background_image');
+    page_banner();
     ?>
-
-    <div class="page-banner">
-      <div
-        class="page-banner__bg-image"
-        style="background-image: url(<?php echo $page_banner_image['sizes']['pageBanner']; ?>"
-      ></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p><?php the_field('page_banner_subtitle'); ?></p>
-        </div>
-      </div>
-    </div>
 
     <div class="container container--narrow page-section">
 
