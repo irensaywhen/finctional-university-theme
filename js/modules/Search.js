62ce7fbe64b2aab7efa442c034597491495cf520
Irina <irensaywhen@gmail.com>
2021-04-09 15:07:33 +0300

    Prevent scrolling on the overlay

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -26,10 +26,12 @@ class Search {
   // 3. methods
   openOverlay() {
     this.searchOverlay.addClass('search-overlay--active');
+    $('body').addClass('body-no-scroll');
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass('search-overlay--active');
+    $('body').removeClass('body-no-scroll');
   }
 }
 
