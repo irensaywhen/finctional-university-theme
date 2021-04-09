3a966126054c448de76588632801845c12c77707
Irina <irensaywhen@gmail.com>
2021-04-09 15:20:50 +0300

    handling keydown events

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -10,10 +10,13 @@ class Search {
      */
     this.openOverlay = this.openOverlay.bind(this);
     this.closeOverlay = this.closeOverlay.bind(this);
+    this.keyPressDispatcher = this.keyPressDispatcher.bind(this);
 
     this.openButton = $('.js-search-trigger');
     this.closeButton = $('.search-overlay__close');
     this.searchOverlay = $('.search-overlay');
+    this.isOverlayOpen = false;
+
     this.events();
   }
 
@@ -21,17 +24,39 @@ class Search {
   events() {
     this.openButton.click(this.openOverlay);
     this.closeButton.click(this.closeOverlay);
+    $(document).on('keydown', this.keyPressDispatcher);
   }
 
   // 3. methods
   openOverlay() {
+    console.log('Open overlay');
+
     this.searchOverlay.addClass('search-overlay--active');
     $('body').addClass('body-no-scroll');
+
+    this.isOverlayOpen = true;
   }
 
   closeOverlay() {
+    console.log('Close overlay');
+
     this.searchOverlay.removeClass('search-overlay--active');
     $('body').removeClass('body-no-scroll');
+
+    this.isOverlayOpen = false;
+  }
+
+  keyPressDispatcher(event) {
+    switch (event.keyCode) {
+      case 83:
+        if (!this.isOverlayOpen) this.openOverlay();
+        break;
+      case 27:
+        if (this.isOverlayOpen) this.closeOverlay();
+        break;
+      default:
+        break;
+    }
   }
 }
 
