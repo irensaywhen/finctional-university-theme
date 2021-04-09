751cef179d1fd8264247a50cf0a6904123b361ef
Irina <irensaywhen@gmail.com>
2021-04-09 15:05:13 +0300

    search class scaffold

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,6 +1,35 @@
+import $ from 'jquery';
+
 class Search {
+  // 1. describe and initiate object
   constructor() {
-    alert('Hello from search');
+    /**
+     * To avoid losing the this context when the methods are called
+     * as a callbacks to the respond of jquery events
+     * we need to bind the context on the initiation step
+     */
+    this.openOverlay = this.openOverlay.bind(this);
+    this.closeOverlay = this.closeOverlay.bind(this);
+
+    this.openButton = $('.js-search-trigger');
+    this.closeButton = $('.search-overlay__close');
+    this.searchOverlay = $('.search-overlay');
+    this.events();
+  }
+
+  // 2. events
+  events() {
+    this.openButton.click(this.openOverlay);
+    this.closeButton.click(this.closeOverlay);
+  }
+
+  // 3. methods
+  openOverlay() {
+    this.searchOverlay.addClass('search-overlay--active');
+  }
+
+  closeOverlay() {
+    this.searchOverlay.removeClass('search-overlay--active');
   }
 }
 
